5193febc765819ca6165f7b712e3ac49c098f744
toussa_a <aurelien.toussaint@epitech.eu>
2017-07-13 17:45:25 +0200

    bug fixes

--- negi.js
+++ negi.js
@@ -44,49 +44,52 @@ client.on('message', message => {
   }
   if (message.content.startsWith(config.prefix + 'heal')) {
     const voiceChannel = message.member.voiceChannel;
-    voiceChannel.join()
+    if (voiceChannel)
+      voiceChannel.join()
       .then(connection => {
         const dispatcher = connection.playFile('./sounds/healing.ogg');
+        console.log("Healing");
       })
       .catch(console.error);
   }
   if (message.content.startsWith(config.prefix + 'play')) {
     let args = message.content.split(' ');
     const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel || voiceChannel.type !== 'voice') return message.reply('Can\'t connect to voice channel.');
+    if (!voiceChannel || voiceChannel.type !== 'voice') return message.reply(' I can\'t connect to a voice channel.');
     voiceChannel.join()
-      .then(connnection => {});
-    let stream = yt(args[1], {
-      filter: 'audioonly',
-    });
-    stream.on('error', function () {
-      message.reply("Je n'ai pas réussi à lire cette vidéo :(");
-      message.guild.voiceConnection.disconnect();
-    });
-    const dispatcher = message.guild.voiceConnection.playStream(stream);
-    let collector = message.channel.createCollector(m => m);
-    collector.on('collect', m => {
-      if (m.content.startsWith(config.prefix + 'pause')) {
-        message.channel.send('paused').then(() => {
-          dispatcher.pause();
+      .then(connnection => {
+        let stream = yt(args[1], {
+          filter: 'audioonly',
         });
-      } else if (m.content.startsWith(config.prefix + 'resume')) {
-        message.channel.send('resumed').then(() => {
-          dispatcher.resume();
+        stream.on('error', function () {
+          message.reply("Je n'ai pas réussi à lire cette vidéo :(");
+          message.guild.voiceConnection.disconnect();
         });
-      } else if (m.content.startsWith(config.prefix + 'volume+')) {
-        if (Math.round(dispatcher.volume * 50) >= 100) return message.channel.send(`Volume: ${Math.round(dispatcher.volume*50)}%`);
-        dispatcher.setVolume(Math.min((dispatcher.volume * 50 + (2 * (m.content.split('+').length - 1))) / 50, 2));
-        message.channel.send(`Volume: ${Math.round(dispatcher.volume*50)}%`);
-      } else if (m.content.startsWith(config.prefix + 'volume-')) {
-        if (Math.round(dispatcher.volume * 50) <= 0) return message.channel.send(`Volume: ${Math.round(dispatcher.volume*50)}%`);
-        dispatcher.setVolume(Math.max((dispatcher.volume * 50 - (2 * (m.content.split('-').length - 1))) / 50, 0));
-        message.channel.send(`Volume: ${Math.round(dispatcher.volume*50)}%`);
-      }
-    });
-    dispatcher.on('end', () => {
-      message.guild.voiceConnection.disconnect();
-    });
+        const dispatcher = message.guild.voiceConnection.playStream(stream, { passes: PASSES });
+        let collector = message.channel.createCollector(m => m);
+        collector.on('collect', m => {
+          if (m.content.startsWith(config.prefix + 'pause')) {
+            message.channel.send('paused').then(() => {
+              dispatcher.pause();
+            });
+          } else if (m.content.startsWith(config.prefix + 'resume')) {
+            message.channel.send('resumed').then(() => {
+              dispatcher.resume();
+            });
+          } else if (m.content.startsWith(config.prefix + 'volume+')) {
+            if (Math.round(dispatcher.volume * 50) >= 100) return message.channel.send(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+            dispatcher.setVolume(Math.min((dispatcher.volume * 50 + (2 * (m.content.split('+').length - 1))) / 50, 2));
+            message.channel.send(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+          } else if (m.content.startsWith(config.prefix + 'volume-')) {
+            if (Math.round(dispatcher.volume * 50) <= 0) return message.channel.send(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+            dispatcher.setVolume(Math.max((dispatcher.volume * 50 - (2 * (m.content.split('-').length - 1))) / 50, 0));
+            message.channel.send(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+          }
+        });
+        dispatcher.on('end', () => {
+          message.guild.voiceConnection.disconnect();
+        });
+      });
   }
 });
 
